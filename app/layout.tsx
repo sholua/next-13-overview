cdca3b7accbd091d697d977f9a8590982f7745ca
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-11-16 11:24:07 +0200

    Post list

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-head-element */
+
+import Link from 'next/link';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">
+              Home
+            </Link>
+            <Link href="/posts">
+              Posts
+            </Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,53 @@
+// import PocketBase from 'pocketbase';
+import Link from "next/link";
+import styles from "./Posts.module.css";
+// import CreateNote from "./Create";
+
+// export const dynamic = 'auto',
+//   dynamicParams = true,
+//   revalidate = 0,
+//   fetchCache = 'auto',
+//   runtime = 'nodejs',
+//   preferredRegion = 'auto'
+
+async function getPosts() {
+  // const db = new PocketBase('http://127.0.0.1:8090');
+  // const result = await db.records.getList('posts');
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/posts/records?page=1&perPage=30",
+    { cache: "no-store" }
+  );
+  const data = await res.json();
+  return data?.items as any[];
+}
+
+export default async function PostsPage() {
+  const posts = await getPosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div className={styles.grid}>
+        {posts?.map((post) => {
+          return <Post key={post.id} post={post} />;
+        })}
+      </div>
+
+      {/* <CreateNote /> */}
+    </div>
+  );
+}
+
+function Post({ post }: any) {
+  const { id, title, content, created } = post || {};
+
+  return (
+    <Link href={`/posts/${id}`}>
+      <div className={styles.note}>
+        <h2>{title}</h2>
+        <h5>{content}</h5>
+        <p>{created}</p>
+      </div>
+    </Link>
+  );
+}
